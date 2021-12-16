6df8ef731466614441fb29dc499752da5e812b4e
Ren159 <k20007kk@aitech.ac.jp>
2021-12-16 14:41:19 +0900

    Eturan.htmlとWeb.pyの枠組みを作成

--- Web.py
+++ Web.py
@@ -2,6 +2,23 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 @app.route('/')
+#アップロード
 def Home():
     return render_template('Home.html')
 
+@app.route('/Eturan')
+#ホーム画面
+def Eturan():
+    return render_template('Eturan.html')
+
+@app.route('/Eturan/Grayscale', methods=["POST"])
+def Grayscale():
+    return render_template('Grayscale.html')
+
+@app.route('/Eturan/Rinkaku', methods=["POST"])
+def Grayscale():
+    return render_template('Rinkaku.html')
+
+@app.route('/Eturan/Waku', methods=["POST"])
+def Grayscale():
+    return render_template('Waku.html')
