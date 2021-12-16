14a0a425559880437310f38faeb4e34397a91869
k20098林直希 <saru.884.nonsk.zamurai@gmail.com>
2021-12-16 15:31:00 +0900

    たぶんOK

--- imageprocessing/imageprocess.py
+++ imageprocessing/imageprocess.py
@@ -1,12 +1,11 @@
 import cv2
 #画像のパスの文字列を引数にとる
 class CannyProcess:
+    th1 = 80
+    th2 = 80
     #画像のパスの文字列を引数にとる 
     def fCanny(self,img) :
         self.img = cv2.imread(img, cv2.IMREAD_GRAYSCALE)
-        canny_img = cv2.Canny(self.img,80,80)
-        return canny_img
-
-img = CannyProcess.fCanny('らっだぁ.jpg')
-cv2.imshow(img,'test')
+        self.canny_img = cv2.Canny(self.img,self.th1,self.th2)
+        return self.canny_img
         
