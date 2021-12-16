dc3e549eba7cf4b0fb79dd17f988b79d40ffcd81
18koki <k-shima.0616.koki@hotmail.com>
2021-12-16 15:57:06 +0900

    Update grayScale.py

--- imageprocessing/grayScale.py
+++ imageprocessing/grayScale.py
@@ -3,7 +3,11 @@ import os
 
 class img_gray():
     def grayscale(self, img_name):
-        im = cv2.imread(f'{img_name}',cv2.IMREAD_GRAYSCALE)
         im_file = os.path.basename(img_name)
+        im = cv2.imread(f'./imageprocessing/input/{im_file}',cv2.IMREAD_GRAYSCALE)    
 
         cv2.imwrite(f'./imageprocessing/grayscale/out_{im_file}',im)
+
+if __name__ == '__main__':
+    a = img_gray()
+    a.grayscale('./input/a.png')
