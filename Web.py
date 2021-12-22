6fa403042a05f16f172588f337548724643303c0
k20034katodaichi <k20034kk@aitech.ac.jp>
2021-12-22 23:25:46 +0900

    時間切れだ...

--- Web.py
+++ Web.py
@@ -1,12 +1,15 @@
 # request フォームから送信した情報を扱うためのモジュール
 from flask import Flask, render_template, request
-
+import cv2
 # 画像のアップロード先のディレクトリ
 UPLOAD_FOLDER = 'imageprocessing/input'
 
 app = Flask(__name__)
-@app.route('/upload', methods=['GET', 'POST'])
-#アップロード
+@app.route('/')
+def Home():
+    return render_template('Home.html')
+
+@app.route('/upload', methods=['POST'])#アップロード
 def Upload():
     if request.method == 'POST':
         img_file = request.files['img_file']
@@ -16,22 +19,22 @@ def Upload():
 def Eturan():
     return render_template('Eturan.html')
 
-@app.route('/grayscale', methods=["POST"])
+@app.route('/grayscale')
 def Grayscale():
     #この中に処理された画像を取得する処理を書く
     return render_template('Grayscale.html')
 
-@app.route('/rinkaku', methods=["POST"])
+@app.route('/rinkaku')
 def Rinkaku():
     #この中に処理された画像を取得する処理を書く
     return render_template('Rinkaku.html')
 
-@app.route('/waku', methods=["POST"])
+@app.route('/waku')
 def Waku():
     #この中に処理された画像を取得する処理を書く
     return render_template('Waku.html')
 
 if __name__ == "__main__":
     # debugモードが不要の場合は、debug=Trueを消してください
-    app.run
+    app.run(debug=True)
 
