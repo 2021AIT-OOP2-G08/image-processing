370acb9b1b0748ba4776e2dc09860ba2b4b9c891
k20034katodaichi <k20034kk@aitech.ac.jp>
2021-12-22 17:52:32 +0900

    微調整１号

--- Web.py
+++ Web.py
@@ -1,37 +1,37 @@
 # request フォームから送信した情報を扱うためのモジュール
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request
 
 # 画像のアップロード先のディレクトリ
 UPLOAD_FOLDER = 'imageprocessing/input'
-# アップロードされる拡張子の制限
-ALLOWED_EXTENSIONS = set(['png'])
-
 
 app = Flask(__name__)
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/upload', methods=['GET', 'POST'])
 #アップロード
-def Home():
+def Upload():
     if request.method == 'POST':
         img_file = request.files['img_file']
     return render_template('Home.html')
 
-@app.route('/Eturan')
-#ホーム画面
+@app.route('/eturan')
 def Eturan():
     return render_template('Eturan.html')
 
-@app.route('/Eturan/Grayscale', methods=["POST"])
+@app.route('/grayscale', methods=["POST"])
 def Grayscale():
     #この中に処理された画像を取得する処理を書く
     return render_template('Grayscale.html')
 
-@app.route('/Eturan/Rinkaku', methods=["POST"])
+@app.route('/rinkaku', methods=["POST"])
 def Rinkaku():
     #この中に処理された画像を取得する処理を書く
     return render_template('Rinkaku.html')
 
-@app.route('/Eturan/Waku', methods=["POST"])
+@app.route('/waku', methods=["POST"])
 def Waku():
     #この中に処理された画像を取得する処理を書く
     return render_template('Waku.html')
+
+if __name__ == "__main__":
+    # debugモードが不要の場合は、debug=Trueを消してください
+    app.run
 
